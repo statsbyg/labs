8d76acbdce3611564232462f9540cf73bce62ac0
Yonathan Redda <yredda@cisco.com>
2016-11-11 23:50:51 -0800

    RecyclerView with ButterKnife

--- RecyclerViewLab/app/src/main/java/com/jonecx/HomeActivity.java
+++ RecyclerViewLab/app/src/main/java/com/jonecx/HomeActivity.java
@@ -2,12 +2,39 @@ package com.jonecx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.my_recycler_view)
+    RecyclerView recyclerView;
+
+    RecyclerView.Adapter recyclerAdapter;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        String[] myDataSet = {"Mercury", "Venus", "Earth", "Mars", "Jupitor", "Saturn"};
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // user a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify and adapter
+        recyclerAdapter = new MyAdapter(myDataSet);
+        recyclerView.setAdapter(recyclerAdapter);
+
     }
 }
---
+++ RecyclerViewLab/app/src/main/java/com/jonecx/MyAdapter.java
@@ -0,0 +1,56 @@
+package com.jonecx;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.jonecx.MyAdapter.ViewHolder;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MyAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+    private String[] mDataset;
+
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+
+        @BindView(R.id.txt_content)
+        TextView mTextView;
+
+        public ViewHolder(View v) {
+            super(v);
+            ButterKnife.bind(this, v);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_one_textbox, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
